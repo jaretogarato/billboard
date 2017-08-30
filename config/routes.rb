5edff271ddd14757737838c054cedd14989857c2
Jared Gallardo <jaredgg@gmail.com>
2017-08-30 16:58:09 -0600

    broken

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'charts#index' # this is our homepage
+
+  resources :charts do 
+    resources :songs
+  end
+
+  # scope 'topics/:topic_id', as: 'topic' do 
+  #   resources :comments, only: [:new, :create]
+  # end 
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,65 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Chart.create(name: 'Billboard Top 100')
+Chart.create(name: 'Billboard Top 50')
+Chart.create(name: 'Billboard Top European')
+
+a = Artist.create(name: "Luis Fonsi & Daddy Yankee Featuring Justin Bieber")
+Song.create(name: "Despacito", artist: a)
+a = Artist.create(name: "Yo Gotti Featuring Nicki Minaj")
+Song.create(name: "Rake It Up", artist: a)
+a = Artist.create(name: "Demi Lovato")
+a = Artist.create(name: "Niall Horan")
+a = Artist.create(name: "Justin Bieber + BloodPop")
+a = Artist.create(name: "DJ Khaled Featuring Rihanna & Bryson Tiller")
+a = Artist.create(name: "Cardi B")
+a = Artist.create(name: "Imagine Dragons")
+a = Artist.create(name: "Charlie Puth")
+a = Artist.create(name: "French Montana Featuring Swae Lee")
+a = Artist.create(name: "Shawn Mendes")
+a = Artist.create(name: "Bruno Mars")
+a = Artist.create(name: "Ed Sheeran")
+
+
+
+
+
+
+
+Song.create(name: "Rake It Up")
+Yo Gotti Featuring Nicki Minaj
+
+Song.create(name: "Sorry Not Sorry")
+Demi Lovato
+
+Song.create(name: "Slow Hands")
+Niall Horan
+
+Song.create(name: "Friends")
+Justin Bieber + BloodPop
+
+Song.create(name: "Wild Thoughts")
+DJ Khaled Featuring Rihanna & Bryson Tiller
+
+Song.create(name: "Bodak Yellow (Money Moves)")
+Cardi B
+
+Song.create(name: "Believer")
+Imagine Dragons
+
+Song.create(name: "Attention")
+Charlie Puth
+
+Song.create(name: "Unforgettable")
+French Montana Featuring Swae Lee
+
+Song.create(name: "There's Nothing Holdin' Me Back")
+Shawn Mendes
+
+Song.create(name: "That's What I Like")
+Bruno Mars
+
+Song.create(name: "Shape Of You")
+Ed Sheeran
